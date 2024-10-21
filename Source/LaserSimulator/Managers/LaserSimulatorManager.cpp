98468e11dc3f2c0d4eae086c60450aaea5fe30d1
Camilo Ule <camiloule21@gmail.com>
2024-10-21 09:28:51 -0500

    Feat (LaserManager): A manager was created to manage the different rules of the simulation.

---
+++ Source/LaserSimulator/Managers/LaserSimulatorManager.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Managers/LaserSimulatorManager.h"
+
+LaserSimulatorManager::LaserSimulatorManager()
+{
+}
+
+LaserSimulatorManager::~LaserSimulatorManager()
+{
+}
---
+++ Source/LaserSimulator/Managers/LaserSimulatorManager.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class LASERSIMULATOR_API LaserSimulatorManager
+{
+public:
+	LaserSimulatorManager();
+	~LaserSimulatorManager();
+};
